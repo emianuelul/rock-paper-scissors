79ee5b23a507cd2731bd5b0dd9e5635eb5c5d39d
emianuelul <emanuel.marin0000@gmail.com>
2025-05-27 18:15:07 +0300

    feat: made player choose and can now lose/win based on his choice

--- code.js
+++ code.js
@@ -1,6 +1,25 @@
-// #1. Generate computer option
-
 const options = ["rock", "paper", "scissors"];
-const computerGuess = options[Math.floor(Math.random() * 3)];
 
-console.log(computerGuess);
+// #1. Generate computer option
+function getComputerChoice(){
+    return computerGuess = options[Math.floor(Math.random() * 3)];
+}
+
+
+console.log(getComputerChoice);
+
+// #2. Take input from the player
+let playerGuess = prompt("Rock, Paper, Scissors! Which one will you choose?");
+
+console.log(playerGuess);
+
+if((playerGuess === "rock" && computerGuess === "paper") ||
+    (playerGuess === "paper" && computerGuess === "scissors") ||
+    (playerGuess === "scissors" && computerGuess === "rock")){
+    alert("I chose " + computerGuess + "! You lose!");
+}
+else if((computerGuess === "rock" && playerGuess === "paper") ||
+    (computerGuess === "paper" && playerGuess === "scissors") ||
+    (computerGuess === "scissors" && playerGuess === "rock")){
+    alert("Impossible! You beat me!");
+}
