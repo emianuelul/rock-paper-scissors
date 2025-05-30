17387a42890ad1b92c997e091bc5f2559e317a97
emianuelul <emanuel.marin0000@gmail.com>
2025-05-30 16:35:44 +0300

    feat: added buttons which let you choose your option; added score that updates after every round

--- code.js
+++ code.js
@@ -1,54 +1,61 @@
 const options = ["rock", "paper", "scissors"];
 
-const number = parseInt(prompt("How many rounds of Rock, Paper, Scissors do you want to play?"));
+let playerChoice;
 
-playGame(number);
 
 function getComputerChoice(){
     return computerGuess = options[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice(){
-    return prompt("Rock, Paper, Scissors! Which one will you choose?");
+let playerScore = 0;
+let computerScore = 0;
+
+function updateScores(){
+    const playerSc = document.querySelector("#playerScore");
+    const computerSc = document.querySelector("#computerScore");
+
+    playerSc.textContent = "Player Score: " + playerScore;
+    computerSc.textContent = "Computer Score: " + computerScore;
 }
 
-function playGame(roundCount){
-    let playerScore = 0;
-    let computerScore = 0;
-
-    function playRound(playerChoice, computerChoice){    
-        playerChoice = playerChoice.toLowerCase();
-
-        if((playerChoice === "rock" && computerChoice === "paper") ||
-            (playerChoice === "paper" && computerChoice === "scissors") ||
-            (playerChoice === "scissors" && computerChoice === "rock")){
-                computerScore++;
-                alert("Player: " + playerScore + " Computer: " + computerScore +  "\n" + "I chose " + computerChoice + "! You lose!");
-        }
-        else if((computerChoice === "rock" && playerChoice === "paper") ||
-            (computerChoice === "paper" && playerChoice === "scissors") ||
-            (computerChoice === "scissors" && playerChoice === "rock")){
-                playerScore++;
-                alert("Player: " + playerScore + " Computer: " + computerScore +  "\n" + "Impossible! You beat me!");
-        }
-        else if(computerChoice === playerChoice){
-                alert("Player: " + playerScore + " Computer: " + computerScore +  "\n" + "Draw!");
-        }
+function playRound(playerChoice, computerChoice){    
+    if((playerChoice === "rock" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "scissors") ||
+        (playerChoice === "scissors" && computerChoice === "rock")){
+            computerScore++;
+            alert("You lose!");
+    }
+    else if((computerChoice === "rock" && playerChoice === "paper") ||
+        (computerChoice === "paper" && playerChoice === "scissors") ||
+        (computerChoice === "scissors" && playerChoice === "rock")){
+            playerScore++;
+            alert("You beat me!");
+    }
+    else if(computerChoice === playerChoice){
+            alert("Draw!");
     }
 
-    for(i = 0; i < roundCount; i++){
-        console.log("Round " + i);
-        const computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        const playerSelection = getHumanChoice();
-        playRound(playerSelection, computerSelection);
-        console.log("Player: " + playerScore + " Computer: " + computerScore);
-    }
-
-    if(playerScore > computerScore){
-        alert("You beat me fair and square.");
-    }
-    else{
-        alert("Better luck next time!");
-    }
+    updateScores();
 }
+
+const optSection = document.querySelector(".options");
+
+optSection.addEventListener('click', (e) =>{
+    switch(e.target.id){
+        case "rock":{
+            playerChoice = "rock";
+            break;
+        }
+        case "paper":{
+            playerChoice = "paper";
+            break;
+        }
+        case "scissors":{
+            playerChoice = "scissors";
+            break;
+        }
+        default:
+            break;
+    }
+    playRound(playerChoice, getComputerChoice());
+})
